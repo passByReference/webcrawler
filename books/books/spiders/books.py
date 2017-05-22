a1c31762537d4b17aaee084da1377c9b088526f9
Technipire <passByReference@users.noreply.github.com>
2017-05-21 20:25:47 -0400

    crawl with selenium

--- books/books/spiders/books.py
+++ books/books/spiders/books.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from time import sleep
-
+import logging
 from scrapy import Spider
 from selenium import webdriver
 from scrapy.selector import Selector
@@ -9,51 +9,13 @@ from scrapy.xlib.pydispatch import dispatcher
 from scrapy import signals
 from selenium.common.exceptions import NoSuchElementException
 
+logging.basicConfig(filename='BooksSpider.log',
+                    filemode='w',  # logging for each run will start afresh
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s')
+logger = logging.getLogger()
 
-class TestSpider(Spider):
-    name = 'TestSpider'
-    allowed_domains = ['books.toscrape.com']
-    homepage_url = 'http://books.toscrape.com/'
-    start_urls = [
-        'http://books.toscrape.com/'
-    ]
-    num_books = 0
-    # parse single page worked
-    def parse(self, response):
-        sel = Selector(response)
-        books = sel.xpath('//h3/a/@href').extract()
-        for book in books:
-            yield Request(response.urljoin(book), callback=self.parse_book)
-
-    def parse_book(self, response):
-        self.logger.info('Parsing new book')
-        print ('book url = ', response.url)
-        sel = Selector(response)
-        title = sel.xpath('//h1/text()').extract()
-        price = sel.xpath('//p[@class="price_color"]/text()').extract()  # \xa3 is the pound sign
-        instock = sel.xpath('//p[@class="instock availability"]/text()').extract()
-        # [u'\n    ', u'\n    \n        In stock (22 available)\n    \n']
-        rating = sel.xpath('//p[contains(@class, "star-rating")]').extract()
-        # t() [u'<p class="star-rating Three">\n        <i class="icon-star"></i>\n
-        #  <i class="icon-star"></i>\n        <i class="icon-star"></i>\n
-        #  <i class="icon-star"></i>\n        <i class="icon-star"></i>\n\n
-        #  <!-- <small><a href="/catalogue/a-light-in-the-attic_1000/reviews/">\n
-        # \n                \n                    0 customer reviews\n
-        #  \n        </a></small>\n         -->\xa0\n\n\n<!-- \n
-        # <a id="write_review" href="/catalogue/a-light-in-the-attic_1000/reviews/add/#addreview" class="btn btn-success btn-sm">\n
-        #  Write a review\n    </a>\n\n --></p>']
-
-        product_description = sel.xpath('//article[@class="product_page"]/p/text()').extract()
-        product_information = sel.xpath('//article[@class="product_page"]/table[@class="table table-striped"]').extract()
-
-        print('title   : ', title)
-        print('price   : ', price)
-        print('instock?: ', instock)
-        print('prd desc: ', product_description)
-        self.num_books += 1
-        print('num_books = ', self.num_books)
-
-
+# spider with selenium
 class BooksSpider(Spider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
@@ -61,23 +23,23 @@ class BooksSpider(Spider):
     num_books = 0;
 
     def start_requests(self):
+        
         self.driver = webdriver.Chrome()
         dispatcher.connect(self.spider_closed, signals.spider_closed)
         self.driver.get('http://books.toscrape.com')
 
         sel = Selector(text=self.driver.page_source)
         categories = sel.xpath('//ul/li/a/@href').extract()
-        print (categories)
+        logger.info(categories)
 
         # each category is crawled
         for category in categories:
-            print ('category = ', category)
+            logger.info ('category = {}'.format(category))
             url = self.homepage_url + category
             self.driver.get(url)
             yield Request(url, callback=self.parse_category)
 
-        print('total books: ', self.num_books)
-
+        logger.info('total books: {}'.format(self.num_books))
 
     def parse_category(self, response):
         self.logger.info('Parsing new Category')
@@ -85,7 +47,7 @@ class BooksSpider(Spider):
 
         for book in books:
             l = book.split('/')
-            print('book link: ', l[-2])
+            logger.debug('book link: {}'.format(l[-2]))
             url = self.homepage_url + 'catalogue/' + l[-2] + '/' + l[-1]
             self.driver.get(url)
             yield Request(url, callback=self.parse_book)
@@ -102,17 +64,17 @@ class BooksSpider(Spider):
                 books = sel.xpath('//h3/a/@href').extract()
                 for book in books:
                     l = book.split('/')
-                    print('book link: ', l[-2])
+                    logger.debug('book link: {}'.format(l[-2]))
                     url = self.homepage_url + 'catalogue/' + l[-2] + '/' + l[-1]
                     yield Request(url, callback=self.parse_book)
             except NoSuchElementException:
-                self.logger.info('No more pages to load')
+                self.logger.error('No more pages to load')
                 self.driver.quit()
                 break
 
     def parse_book(self, response):
-        self.logger.info('Parsing new book')
-        print ('book url = ', response.url)
+        self.logger.debug('Parsing new book')
+        logger.debug ('book url = ', response.url)
         sel = Selector(response)
         title = sel.xpath('//h1/text()').extract()
         price = sel.xpath('//p[@class="price_color"]/text()').extract()  # \xa3 is the pound sign
@@ -131,12 +93,12 @@ class BooksSpider(Spider):
         product_description = sel.xpath('//article[@class="product_page"]/p/text()').extract()
         product_information = sel.xpath('//article[@class="product_page"]/table[@class="table table-striped"]').extract()
 
-        print('title   : ', title)
-        print('price   : ', price)
-        print('instock?: ', instock)
-        print('prd desc: ', product_description)
+        logger.debug('title   : {}'.format(title))
+        logger.debug('price   : {}'.format(price))
+        logger.debug('instock?: {}'.format(instock))
+        logger.debug('prd desc: {}'.format(product_description))
         self.num_books += 1
-        print('num_books = ', self.num_books)
+        logger.debug('num_books = {} '.format(self.num_books))
 
     def spider_closed(self):
         self.driver.close()
